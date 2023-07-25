eaa81d83468dde6e27a8f731da9e5fbfd7328d92
kobzpanel <135472712+kobzpanel@users.noreply.github.com>
2023-07-25 07:42:24 +0300

    Create panel update token.js

---
+++ panel update token.js
@@ -0,0 +1,302 @@
+{
+     "tktunnel" : [
+    {
+     "User":"tktunnel",
+     "status":"online",
+     "password":"tktunnel",
+     "domain":"tktunnelvip.xyz",
+     "hash":"d2fae63dc781127f4b56"
+    }
+  ],
+  "bhtunnelplus" : [
+    {
+     "User":"bhtunnelplus",
+     "status":"online",
+     "password":"bhtunnelplus",
+     "domain":"bhtunnelplus.xyz",
+     "hash":"9807148a4360e0d066b4"
+    }
+  ],
+   "lemonvipvpn" : [
+    {
+     "User":"lemonvipvpn",
+     "status":"online",
+     "password":"lemonvipvpn",
+     "domain":"lemonvipvpn.site",
+     "hash":"7c64c3496886ee57859d"
+    }
+  ],
+  "alamindev" : [
+    {
+     "User":"alamindev",
+     "status":"online",
+     "password":"alamindev",
+     "domain":"bhtunnelplus.xyz",
+     "hash":"9807148a4360e0d066b4"
+    }
+  ],
+ "newuser" : [
+    {
+     "User":"newuser",
+     "status":"online",
+     "password":"newuser",
+     "domain":"jsonpanels.xyz",
+     "hash":"c9f4849846ea058f943e"
+    }
+  ],
+  "rgpronet" : [
+    {
+     "User":"rgpronet",
+     "status":"online",
+     "password":"rgpronet",
+     "domain":"rgtunnelvpn.xyz",
+     "hash":"88587a1f738105ff94aa"
+    }
+  ],
+  "guptatunnel" : [
+    {
+     "User":"guptatunnel",
+     "status":"online",
+     "password":"guptatunnel",
+     "domain":"guptatunnel.xyz",
+     "hash":"06a93b63773598041af2"
+    }
+  ],
+  "mastarvpn" : [
+    {
+     "User":"mastarvpn",
+     "status":"online",
+     "password":"mastarvpn",
+     "domain":"mastarvpn.xyz",
+     "hash":"23a0c6a4d3fc8343f1a6"
+    }
+  ],
+  "orangemastervpn" : [
+    {
+     "User":"orangemastervpn",
+     "status":"online",
+     "password":"orangemastervpn",
+     "domain":"orangemastervpnbd.xyz",
+     "hash":"82c9bdcb75eb109c1943"
+    }
+  ],
+  "abdullahtunnelvip" : [
+    {
+     "User":"abdullahtunnelvip",
+     "status":"online",
+     "password":"abdullahtunnelvip",
+     "domain":"abdullahtunnelvip.xyz",
+     "hash":"d3aa032499acf06de35f"
+    }
+  ],
+  "bm" : [
+    {
+     "User":"bm",
+     "status":"online",
+     "password":"bm",
+     "domain":"bmviptunnelvpn.xyz",
+     "hash":"4b679fc8d74589a83780"
+     
+    }
+  ],
+  "axnetvip" : [
+    {
+     "User":"axnetvip",
+     "status":"online",
+     "password":"axnetvip",
+     "domain":"axnetvip.xyz",
+     "hash":"171b04e8e132722e2df9"
+    }
+  ],
+  "alldatavip" : [
+    {
+     "User":"alldatavip",
+     "status":"online",
+     "password":"alldatavip",
+     "domain":"alldatavip.xyz",
+     "hash":"c533c978dcae26f9faa5"
+    }
+  ],
+  "tecnepal" : [
+    {
+     "User":"tecnepal",
+     "status":"online",
+     "password":"tecnepal",
+     "domain":"tecnepal.com",
+     "hash":"fc89caff47c698668f4e"
+     
+    }
+  ],
+  "dxplus" : [
+    {
+     "User":"dxplus",
+     "status":"online",
+     "password":"dxplus",
+     "domain":"dxplus.xyz",
+     "hash":"92ba47e9302a1c4ece8c"
+     
+    }
+  ],
+  "iconvipnet" : [
+    {
+     "User":"iconvipnet",
+     "status":"online",
+     "password":"iconvipnet",
+     "domain":"iconvipnet.xyz",
+     "hash":"8b8f4b9cbb19c5de8692"
+    }
+  ],
+  "atelvpn" : [
+    {
+     "User":"atelvpn",
+     "status":"online",
+     "password":"atelvpn",
+     "domain":"atelvpn.net",
+     "hash":"6fbe835b3d34410ec770"
+    }
+  ],
+  "orangemaster" : [
+    {
+     "User":"orangemaster",
+     "status":"online",
+     "password":"orangemaster",
+     "domain":"orangemaster.xyz",
+     "hash":"7f2c846af69f61265bb5"
+    }
+  ],
+  "appsdev" : [
+    {
+     "User":"appsdev",
+     "status":"online",
+     "password":"appsdev",
+     "domain":"orangemaster.xyz",
+     "hash":"7f2c846af69f61265bb5"
+    }
+  ],
+  "rtvpn" : [
+    {
+     "User":"rtvpn",
+     "status":"online",
+     "password":"rtvpn",
+     "domain":"rtvpn.xyz",
+     "hash":"5c4ebeff5922b0dbde07"
+    }
+  ],
+  "rdxvipnet" : [
+    {
+     "User":"rdxvipnet",
+     "status":"online",
+     "password":"rdxvipnet",
+     "domain":"rdxvipnet.xyz",
+     "hash":"65d50dcd6ac130f10cdf"
+    }
+  ],
+  "tpsmartnetvip" : [
+    {
+     "User":"tpsmartnetvip",
+     "status":"online",
+     "password":"tpsmartnetvip",
+     "domain":"tpsmartnetvip.xyz",
+     "hash":"2283840b01bf51d31d8b"
+    }
+  ],
+  "finenetvpn" : [
+    {
+     "User":"finenetvpn",
+     "status":"online",
+     "password":"finenetvpn",
+     "domain":"finenetvpn.xyz",
+     "hash":"6ad6faf63470ba8fcb6e"
+    }
+  ],
+  "Arabic" : [
+    {
+     "User":"Arabic",
+     "status":"online",
+     "password":"Arabic",
+     "domain":"arabicnetvip.xyz",
+     "hash":"0349920311c88966fdc1"
+    }
+  ],
+  "tangailbrand" : [
+    {
+     "User":"tangailbrand",
+     "status":"online",
+     "password":"tangailbrand",
+     "domain":"tangailbrand.xyz",
+     "hash":"a2fc1a309001c1a94ea4"
+    }
+  ],
+  "soudi7gnet" : [
+    {
+     "User":"soudi7gnet",
+     "status":"online",
+     "password":"soudi7gnet",
+     "domain":"soudi7gnet.xyz",
+     "hash":"2a607e1954c59c1dabc6"
+    }
+  ],
+  "jumnanetvip" : [
+    {
+     "User":"jumnanetvip",
+     "status":"online",
+     "password":"jumnanetvip",
+     "domain":"jumnanetvip.xyz",
+     "hash":"7aa1cf8e8b3d8cf40ad7"
+    }
+  ],
+  "jumnanet" : [
+    {
+     "User":"jumnanet",
+     "status":"online",
+     "password":"jumnanet",
+     "domain":"jumnanetvip.xyz",
+     "hash":"9d64f1be15ffd75c0447"
+    }
+  ],
+  "abcudp" : [
+    {
+     "User":"abcudp",
+     "status":"online",
+     "password":"abcudp",
+     "domain":"abcudp.xyz",
+     "hash":"64bca5954553e3520468"
+    }
+  ],
+  "sumonvpn" : [
+    {
+     "User":"sumonvpn",
+     "status":"online",
+     "password":"sumonvpn",
+     "domain":"sumonvpn.xyz",
+     "hash":"c7119685e8258ea721a1"
+    }
+  ],
+  "ahdev" : [
+    {
+     "User":"ahdev",
+     "status":"online",
+     "password":"ahdev",
+     "domain":"audp.xyz",
+     "hash":"ee9e492439e28bd2b20"
+    }
+  ],
+  "jsfastvpn" : [
+    {
+     "User":"jsfastvpn",
+     "status":"online",
+     "password":"jsfastvpn",
+     "domain":"jsfastvpn.xyz",
+     "hash":"ee9e4924391e28bd2b20"
+    }
+  ],
+  "ikingvipvpn" : [
+    {
+     "User":"ikingvipvpn",
+     "status":"online",
+     "password":"ikingvipvpn",
+     "domain":"ikingvipvpn.xyz",
+     "hash":"1ae991afa4502026ecab"
+    }
+  ]
+}
